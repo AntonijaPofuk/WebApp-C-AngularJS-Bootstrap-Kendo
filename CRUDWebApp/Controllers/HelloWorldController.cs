354b2dfcb4871db0aec10a8de1c79271df871c5d
AntonijaP <antonija.pofuk@gmail.com>
2021-01-01 20:18:24 +0100

    Added Prototype

--- CRUDWebApp/Controllers/HelloWorldController.cs
+++ CRUDWebApp/Controllers/HelloWorldController.cs
@@ -3,6 +3,7 @@ using CRUDWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using static CRUDWebApp.Controllers.Builder;
+using static CRUDWebApp.Controllers.Prototype;
 
 namespace CRUDWebApp.Controllers
 {
@@ -39,19 +40,45 @@ class Employee
             //    cfg.AddProfile<MyMappingProfile>();
             //});
 
-            /// Builder Design Pattern Demo
+            // Builder Design Pattern Demo
             var vehicleCreator = new VehicleCreator(new HeroBuilder());
             vehicleCreator.CreateVehicle();
             var vehicle = vehicleCreator.GetVehicle();
             vehicle.ShowInfo();
-
             Console.WriteLine("---------------------------------------------");
-
             vehicleCreator = new VehicleCreator(new HondaBuilder());
             vehicleCreator.CreateVehicle();
             vehicle = vehicleCreator.GetVehicle();
-            vehicle.ShowInfo();
+            //vehicle.ShowInfo();
+
+
+            //Prototype
+            Developer dev = new Developer();
+            dev.Name = "Ann";
+            dev.Role = "Team Leader";
+            dev.PreferredLanguage = "C#";
+
+            Developer devCopy = (Developer) dev.Clone();
+            devCopy.Name = "Anna"; //Not mention Role and PreferredLanguage, it will copy above
+
+            Console.WriteLine(dev.GetDetails());
+            Console.WriteLine(devCopy.GetDetails());
+
+            Typist typist = new Typist();
+            typist.Name = "Beta";
+            typist.Role = "Typist";
+            typist.WordsPerMinute = 120;
+
+            Typist typistCopy = (Typist) typist.Clone();
+            typistCopy.Name = "Betty";
+            typistCopy.WordsPerMinute = 115;//Not mention Role, it will copy above
+
+            Console.WriteLine(typist.GetDetails());
+            Console.WriteLine(typistCopy.GetDetails());
             return View();
+
+           
+
         }
 
         public IActionResult Welcome(string name, int numTimes = 1)
